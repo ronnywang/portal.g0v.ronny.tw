4dc2f34a627bd1df5f89540f8d649b1d3b6a4af4
Ronny Wang <ronnywang@gmail.com>
2013-11-13 10:55:26 +0800

    加上可以取得名稱的功能

--- webdata/models/CountryGroup.php
+++ webdata/models/CountryGroup.php
@@ -12,23 +12,37 @@ class CountryGroup extends Pix_Table
     }
 
     protected static $_pool = null;
+    protected static $_name_pool = null;
 
-    public static function getCode($name)
+    protected static function initPool()
     {
         if (is_null(self::$_pool)) {
             self::$_pool = array();
             foreach (CountryGroup::search(1) as $group) {
                 self::$_pool[$group->name] = $group->id;
+                self::$_name_pool[$group->id] = $group->name;
             }
         }
+    }
 
-        if (!array_key_exists($name, self::$_pool)) {
+    public static function getCode($name, $auto_create = true)
+    {
+        self::initPool();
+
+        if ($auto_create and !array_key_exists($name, self::$_pool)) {
             $group = CountryGroup::insert(array(
                 'name' => $name,
             ));
             self::$_pool[$name] = $group->id;
+            self::$_name_pool[$group->id] = $name;
         }
 
         return self::$_pool[$name];
+    }
+
+    public static function getName($id)
+    {
+        self::initPool();
+        return self::$_name_pool[$id];
     }
 }
--- webdata/models/UnitGroup.php
+++ webdata/models/UnitGroup.php
@@ -12,23 +12,38 @@ class UnitGroup extends Pix_Table
     }
 
     protected static $_pool = null;
+    protected static $_name_pool = null;
 
-    public static function getCode($name)
+    protected static function initPool()
     {
         if (is_null(self::$_pool)) {
             self::$_pool = array();
             foreach (UnitGroup::search(1) as $group) {
                 self::$_pool[$group->name] = $group->id;
+                self::$_name_pool[$group->id] = $group->name;
             }
         }
+    }
+
+
+    public static function getCode($name)
+    {
+        self::initPool();
 
         if (!array_key_exists($name, self::$_pool)) {
             $group = UnitGroup::insert(array(
                 'name' => $name,
             ));
             self::$_pool[$name] = $group->id;
+            self::$_name_pool[$group->id] = $name;
         }
 
         return self::$_pool[$name];
+    }
+
+    public static function getName($id)
+    {
+        self::initPool();
+        return self::$_name_pool[$id];
     }
 }
