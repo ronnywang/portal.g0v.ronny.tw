edb8d69c458f67e00a315f48c658b328f3f4c526
Ronny Wang <ronnywang@gmail.com>
2013-11-20 17:27:18 +0800

    增加匯入 good id 的 script

---
+++ webdata/scripts/import-id.php
@@ -0,0 +1,30 @@
+<?php
+
+include(__DIR__ . '/../init.inc.php');
+$fp = fopen($_SERVER['argv'][1], 'r');
+fgetcsv($fp);
+$columns = array(
+    'id',
+    'parent_id',
+    'name',
+    'ename',
+);
+
+$values = array();
+while ($row = fgetcsv($fp)) {
+    $value = array(
+        $row[0],
+        floor($row[0] / 100),
+        iconv('UTF-8', 'UTF-8', $row[1]),
+        $row[2],
+    );
+    $values[] = $value;
+    if (count($values) > 1000) {
+        GoodId::bulkInsert($columns, $values);
+        $values = array();
+    }
+}
+if (count($values)) {
+    GoodId::bulkInsert($columns, $values);
+    $values = array();
+}
