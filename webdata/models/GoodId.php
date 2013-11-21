84ed10b541409d775fa03222b93bc810460646cf
Ronny Wang <ronnywang@gmail.com>
2013-11-21 11:46:27 +0800

    加上 GoodId->id()

--- webdata/models/GoodId.php
+++ webdata/models/GoodId.php
@@ -1,11 +1,30 @@
 <?php
 
+class GoodIdRow extends Pix_Table_Row
+{
+    public function id()
+    {
+        if ($id < 100) {
+            return sprintf('%02d', $this->id);
+        } elseif ($id < 10000) {
+            return sprintf('%04d', $this->id);
+        } elseif ($id < 1000000) {
+            return sprintf('%06d', $this->id);
+        } elseif ($id < 100000000) {
+            return sprintf('%08d', $this->id);
+        } else {
+            return sprintf('%010d', $this->id);
+        }
+    }
+}
+
 class GoodId extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'good_id';
         $this->_primary = 'id';
+        $this->_rowClass = 'GoodIdRow';
 
         $this->_columns['id'] = array('type' => 'bigint', 'unsigned' => true);
         $this->_columns['parent_id'] = array('type' => 'int');
