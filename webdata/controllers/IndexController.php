f943ef295537cf067d6540cd0423786ba9a25ca0
Ronny Wang <ronnywang@gmail.com>
2013-11-21 12:15:39 +0800

    加上產品 ID 瀏覽功能

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -5,4 +5,14 @@ class IndexController extends Pix_Controller
     public function indexAction()
     {
     }
+
+    public function goodidAction()
+    {
+        list(, /*index*/, /*goodid*/, $goodid) = explode('/', $this->getURI());
+        if (!$goodid_obj = GoodId::find(intval($goodid))) {
+            return $this->redirect('/');
+        }
+
+        $this->view->goodid = $goodid_obj;
+    }
 }
