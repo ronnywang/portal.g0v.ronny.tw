5a0a4bc263e090376cea179cd95d53a9fb11313b
Ronny Wang <ronnywang@gmail.com>
2017-06-13 12:13:12 +0800

    拿掉 pool cache ，因為國家數量太多這樣反而變慢

--- webdata/models/CountryGroup.php
+++ webdata/models/CountryGroup.php
@@ -11,38 +11,20 @@ class CountryGroup extends Pix_Table
         $this->_columns['name'] = array('type' => 'varchar', 'size' => 32);
     }
 
-    protected static $_pool = null;
-    protected static $_name_pool = null;
-
-    protected static function initPool()
-    {
-        if (is_null(self::$_pool)) {
-            self::$_pool = array();
-            foreach (CountryGroup::search(1) as $group) {
-                self::$_pool[$group->name] = $group->id;
-                self::$_name_pool[$group->id] = $group->name;
-            }
-        }
-    }
-
     public static function getCode($name, $auto_create = true)
     {
-        self::initPool();
-
-        if ($auto_create and !array_key_exists($name, self::$_pool)) {
+        $group = CountryGroup::find_by_name($name);
+        if ($auto_create and !$group) {
             $group = CountryGroup::insert(array(
                 'name' => $name,
             ));
-            self::$_pool[$name] = $group->id;
-            self::$_name_pool[$group->id] = $name;
         }
 
-        return self::$_pool[$name];
+        return $group->id;
     }
 
     public static function getName($id)
     {
-        self::initPool();
-        return self::$_name_pool[$id];
+        return CountryGroup::find($id)->name;
     }
 }
